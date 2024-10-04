461550bf64776e9aa16052e8db58061a72826eba
balamurugan <balamurugan.sekar@icloud.com>
2024-10-04 15:06:04 +0530

    added backend

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // Import Firestore
+import { getStorage } from "firebase/storage"; // Import Storage
+import { getFunctions, httpsCallable } from "firebase/functions"; // Importing getFunctions and httpsCallable
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDzSuU7XZhwPNbX8dRh7MiClK9213nobZQ",
+    authDomain: "segatfin.firebaseapp.com",
+    projectId: "segatfin",
+    storageBucket: "segatfin.appspot.com",
+    messagingSenderId: "781824355902",
+    appId: "1:781824355902:web:47bd665a92206d4a22eb6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app); // Initialize Firestore
+
+// Initialize Firebase Storage
+const storage = getStorage(app); // Initialize Storage
+// Initialize Firebase
+
+const functions = getFunctions(app); // Exporting the functions instance
+const myFunction = httpsCallable(functions, 'parseQuestion');
+
+export { db, storage, myFunction }; // Export Firestore and Storage instances for use in other files
